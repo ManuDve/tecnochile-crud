0a432e2574c3ebb8a418d881f0706bc765fdfb01
GonzaloAG <123482658+GonzaloAG@users.noreply.github.com>
2023-03-29 13:05:40 -0300

    validaciones (#5)
    Co-authored-by: Manuel Alfaro <m.alfaro@live.cl>

---
+++ js/validaciones.js
@@ -0,0 +1,32 @@
+function validarCampos() {
+    let nombre = document.getElementById("nombre-actual").value;
+    let precio = document.getElementById("precio-actual").value;
+    let stock = document.getElementById("stock-actual").value;
+  
+    if (nombre.length > 50) {
+      alert("El nombre no puede tener más de 50 caracteres");
+      return false;
+    }
+  
+    if (precio === "" || isNaN(precio)) {
+      alert("El precio debe ser un número válido");
+      return false;
+    }
+  
+    if (precio % 1 !== 0 || precio < 0 || precio > 2147483647) {
+      alert("El precio debe ser un número entero válido");
+      return false;
+    }
+  
+    if (stock === "" || isNaN(stock)) {
+      alert("El stock debe ser un número válido");
+      return false;
+    }
+  
+    if (stock % 1 !== 0 || stock < 0 || stock > 2147483647) {
+      alert("El stock debe ser un número entero válido");
+      return false;
+    }
+  
+    return true;
+  }
